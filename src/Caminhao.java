fb78fa494c219402ab2ec311b306622544e960a6
f6206672 Leonardo Clemente de Figueiredo Silva <leonardo.clemente@bb.com.br>
2022-07-27 17:53:21 -0300

    Implementado builder à classe caminhao

--- src/Caminhao.java
+++ src/Caminhao.java
@@ -1,12 +1,41 @@
 import java.util.Random;
 
 public class Caminhao {
+    private Fazenda fazenda;
     private Integer capacidade;
     private Boolean cheio;
 
-    public Caminhao() {
-        this.capacidade = new Random().ints(4, 16).findFirst().getAsInt();
-        this.cheio = false;
+    public Caminhao(Builder builder) {
+        this.fazenda = builder.fazenda;
+        this.capacidade = builder.capacidade;
+        this.cheio = builder.cheio;
+    }
+
+    public static class Builder {
+
+        private Fazenda fazenda;
+        private Integer capacidade;
+        private Boolean cheio;
+
+        public Builder fazenda(Fazenda fazenda) {
+            this.fazenda = fazenda;
+            return this;
+        }
+
+        public Builder capacidade() {
+            this.capacidade = new Random().ints(4, 16).findFirst().getAsInt();
+            return this;
+        }
+
+        public Builder cheio(boolean ischeio) {
+            this.cheio = ischeio;
+            return this;
+        }
+
+        public Caminhao build() {
+            return new Caminhao(this);
+        }
+
     }
 
     public void encher() {
