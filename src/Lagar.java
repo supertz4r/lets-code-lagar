c205301adf9dfb234e412fc4ed50540ea7edf669
Jose Erivaldo Oliveira Junior <jose.erivaldo@bb.com.br>
2022-07-27 16:00:57 -0300

    Implementacao inicial do Lagar

--- src/Lagar.java
+++ src/Lagar.java
@@ -1,4 +1,54 @@
-public class Lagar {
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
 
-    
+public class Lagar implements Callable<Boolean>{
+
+    private static BlockingQueue<Caminhao> filaCaminhoes = new ArrayBlockingQueue<>(12);
+    private boolean isCapacidadeMaxima = false;
+    private boolean isRecepcaoProcessada = false;
+    private double tempoEspera;
+
+    public Lagar() {
+    }
+
+    public boolean getIsCapacidadeMaxima() {
+        return isCapacidadeMaxima;
+    }
+
+    public boolean getIsRecepcaoProcessada() {
+        return isRecepcaoProcessada;
+    }
+
+    public int getTamanhoFila() {
+        return filaCaminhoes.size();
+    }
+
+    public void enfileraCaminhao(Caminhao caminhao) {
+        if (filaCaminhoes.remainingCapacity() == 0) {
+            isCapacidadeMaxima = true;
+            return;
+        }
+        filaCaminhoes.add(caminhao);
+    }
+
+    public double processaRecepcao() {
+        try {
+            Caminhao primeiroCaminhaoDaFila = filaCaminhoes.take();
+            tempoEspera = primeiroCaminhaoDaFila.getCapacidade() / 2; //pois 2 segundos corresponde a 4 toneladas e podemos ter de 4 até 16 toneladas
+            isRecepcaoProcessada = true;
+            return tempoEspera;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Boolean call() throws Exception {
+        long tempoEsperaEmMilis = (long) processaRecepcao() * 1000;
+        Thread.sleep(tempoEsperaEmMilis);
+        return true;
+    }
 }
+
+
