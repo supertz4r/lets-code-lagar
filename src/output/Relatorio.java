aa30ebc5cb171865fd2e6fc6ef353ce5291159fc
ThaisMK <thais.konishi@gmail.com>
2022-07-29 19:09:23 -0300

    reescrevendo metodos de escrita do Relatorio

--- src/output/Relatorio.java
+++ src/output/Relatorio.java
@@ -4,25 +4,44 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Relatorio {
 
-    public boolean escreveRelatorio(String nomeRelatorio, String conteudo) {
+    private List<String> linhas = new ArrayList<>();
 
-        try (
-                FileWriter criaRelatorio = new FileWriter(nomeRelatorio, true);
-                BufferedWriter buffer = new BufferedWriter(criaRelatorio);
-                PrintWriter escreveRelatorio = new PrintWriter(buffer);) {
-            escreveRelatorio.append(conteudo);
-            escreveRelatorio.println();
-            return true;
-
-        } catch (IOException e) {
-
-            e.printStackTrace();
-            return false;
-        }
-
+    public List<String> getLinhas() {
+        return this.linhas;
     }
 
+    public void setLinhas(String linha) {
+        this.linhas.add(linha);
+    }
+
+    public void escreveRelatorio(String nomeRelatorio, List<String> linhas) throws IOException {
+
+        // try (
+        // FileWriter criaRelatorio = new FileWriter(nomeRelatorio, true);
+        // BufferedWriter buffer = new BufferedWriter(criaRelatorio);
+        // PrintWriter escreveRelatorio = new PrintWriter(buffer);) {
+        // escreveRelatorio.append(conteudo);
+        // escreveRelatorio.println();
+        // return true;
+
+        // } catch (IOException e) {
+
+        // e.printStackTrace();
+        // return false;
+        // }
+
+        Files.write(Path.of("src/output/" + nomeRelatorio), linhas);
+    }
+
+    public List<String> adicionaLinha(String linha) {
+
+        return null;
+    }
 }
